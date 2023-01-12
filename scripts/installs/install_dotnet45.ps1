41b49e74cc664e38ae00527193d700c36c2b4cc8
AngelKing86 <47644286+AngelKing86@users.noreply.github.com>
2023-01-12 10:52:09 +0100

    Solution for local preload - files needs some cleanup
    Preload via build.ps1 script, build.sh needs some updates
    Download via packer http in provisioners phase

--- scripts/installs/install_dotnet45.ps1
+++ scripts/installs/install_dotnet45.ps1
@@ -8,10 +8,17 @@ function LogWrite {
  
 LogWrite "Downloading dotNet 4.5.1"
 try {
-    (New-Object System.Net.WebClient).DownloadFile('http://files.network-pro.de/metasploitable3/NDP451-KB2858728-x86-x64-AllOS-ENU.exe', 'C:\Windows\Temp\dotnet.exe')
+    #(New-Object System.Net.WebClient).DownloadFile('http://files.network-pro.de/metasploitable3/NDP451-KB2858728-x86-x64-AllOS-ENU.exe', 'C:\Windows\Temp\dotnet.exe')
+    
+    #Download from HTTP works, but the ENV Vars are not in the vm, so ip and port is hardcoded
+    #(New-Object System.Net.WebClient).DownloadFile('http://{{ .HTTPIP }}:{{ .HTTPPort }}/NDP451-KB2858728-x86-x64-AllOS-ENU.exe', 'C:\Windows\Temp\dotnet.exe')
+    try { (New-Object System.Net.WebClient).DownloadFile('http://10.0.2.2:9001/NDP451-KB2858728-x86-x64-AllOS-ENU.exe', 'C:\Windows\Temp\dotnet.exe') }catch {echo $_.Exception }
+    try { (New-Object System.Net.WebClient).DownloadFile('http://10.0.2.3:9001/NDP451-KB2858728-x86-x64-AllOS-ENU.exe', 'C:\Windows\Temp\dotnet.exe') }catch {echo $_.Exception }
+
 } catch {
     LogWrite $_.Exception | Format-List -force
     LogWrite "Failed to download file."
+    Write-Host "Failed to download file."
 }
 
 LogWrite "Starting installation process..."
--- scripts/installs/install_wmf.ps1
+++ scripts/installs/install_wmf.ps1
@@ -8,10 +8,17 @@ function LogWrite {
  
 LogWrite "Downloading Windows Management Framework 5.0"
 try {
-    (New-Object System.Net.WebClient).DownloadFile('http://files.network-pro.de/metasploitable3/Win7AndW2K8R2-KB3134760-x64.msu', 'C:\Windows\Temp\wmf.msu')
+    #(New-Object System.Net.WebClient).DownloadFile('http://files.network-pro.de/metasploitable3/Win7AndW2K8R2-KB3134760-x64.msu', 'C:\Windows\Temp\wmf.msu')
+
+    #Download from HTTP works, but the ENV Vars are not in the vm, so ip and port is hardcoded
+    #(New-Object System.Net.WebClient).DownloadFile('http://{{ .HTTPIP }}:{{ .HTTPPort }}/Win7AndW2K8R2-KB3134760-x64.msu', 'C:\Windows\Temp\wmf.msu')
+    try { (New-Object System.Net.WebClient).DownloadFile('http://10.0.2.2:9001/Win7AndW2K8R2-KB3134760-x64.msu', 'C:\Windows\Temp\wmf.msu') }catch {echo $_.Exception }
+    try { (New-Object System.Net.WebClient).DownloadFile('http://10.0.2.3:9001/Win7AndW2K8R2-KB3134760-x64.msu', 'C:\Windows\Temp\wmf.msu') }catch {echo $_.Exception }
+
 } catch {
     LogWrite $_.Exception | Format-List -force
     LogWrite "Failed to download file."
+    Write-Host "Failed to download file."
 }
 
 LogWrite "Starting installation process..."
